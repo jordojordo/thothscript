80fb62e86fac622c46de14cdd3c8e16a10dddda0
jordojordo <jordonleach@gmail.com>
2024-05-31 06:28:27 -0400

    Fix NodeJS namespace error

--- src/stores/useToolConfigStore.ts
+++ src/stores/useToolConfigStore.ts
@@ -25,7 +25,6 @@ export const useToolConfigStore = defineStore('toolConfig', {
     runOpts:     {
       input:        '',
       disableCache: false,
-      cacheDir:     '',
       quiet:        false,
       chdir:        '',
       subTool:      '',
